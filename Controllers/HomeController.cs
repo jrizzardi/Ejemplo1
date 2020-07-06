c060b40f221104e4ba97b9d1d84dfae119b4b247
jrizzardi <jrizzardi@gmail.com>
2020-07-06 16:25:06 -0300

    1er commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TablaChina.Models;
+
+
+namespace TablaChina.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LoadData()
+        {
+            Item item = new Item();
+            item.id = 1;
+            item.name = "Item 1";
+            item.Price = 10;
+
+            return Json(item);
+            
+        }
+
+        public IActionResult ShowData()
+        {
+            return Json("test");
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TablaChina.Models
+{
+    public class Item
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Required Name")]
+        public string name { get; set; }
+
+
+        [Required(ErrorMessage = "Required Price")]
+        public double Price { get; set; }
+
+    }
+}
+
